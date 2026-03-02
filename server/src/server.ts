21f2ec545ec76209851e365056623c88b22aa57b
Max Kollmeyer <max.kollmeyer@gmail.com>
2026-03-02 12:51:53 -0500

    Fixed error with client server port

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,19 @@
-import express, { Application, Request, Response } from "express"
+import express from "express"
 import cors from "cors"
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const app: Application = express();
-const port: string | number = process.env.port || 3001;
+const app: express.Application = express();
+const port: number = parseInt(process.env.PORT || '3001', 10);
 
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
     credentials: true,
 }));
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello World');
 });
 
