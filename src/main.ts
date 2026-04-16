95759df34d5b420f6f881dd2382ad88f152ce778
mkollmeyer <max.kollmeyer@gmail.com>
2026-04-15 22:08:37 -0500

    Fixed depenency errors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
-import { get_word, create_word, guess_word } from './router/wordRoutes.ts'
-import { update_score } from './router/statRoutes.ts'
 
-
-console.log(guess_word('crant', 'crane'));
-
-createApp(App).mount('#app');
+createApp(App).mount('#app')
