f86da8feaf4d44b538e969c6b27753c2b2daeb17
mkollmeyer <max.kollmeyer@gmail.com>
2026-04-26 20:21:57 -0500

    removed test

--- util/date.ts
+++ util/date.ts
@@ -2,10 +2,9 @@ export function date_convert(date: Date): string {
 	const days = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 	const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
 		            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-	const min = date.getMinutes();
     const day = days[date.getUTCDay()];
 	const month = months[date.getUTCMonth()];
 	const dayOfMonth = date.getUTCDate().toString().padStart(2, '0');
 	const year = date.getUTCFullYear();
-    return `${min} ${day} ${month} ${dayOfMonth} ${year}`;
+    return `${day} ${month} ${dayOfMonth} ${year}`;
 };
