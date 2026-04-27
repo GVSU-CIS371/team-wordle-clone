a0f3caf370673004e4505a0138774f2e50a4c4f9
mkollmeyer <max.kollmeyer@gmail.com>
2026-04-27 10:48:01 -0500

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/team-wordle-clone/',
   plugins: [
     vue(),
     vueDevTools(),
