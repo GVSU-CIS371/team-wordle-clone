9cc83ea5825a48609ebeec6e2d05d596f63b5b82
mkollmeyer <max.kollmeyer@gmail.com>
2026-04-27 19:59:11 -0500

    Changed date structure and used wordle dictionary

--- util/date.ts
+++ util/date.ts
@@ -1,10 +1,3 @@
 export function date_convert(date: Date): string {
-	const days = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-	const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-		            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    const day = days[date.getUTCDay()];
-	const month = months[date.getUTCMonth()];
-	const dayOfMonth = date.getUTCDate().toString().padStart(2, '0');
-	const year = date.getUTCFullYear();
-    return `${day} ${month} ${dayOfMonth} ${year}`;
+    return date.toLocaleDateString('en-US', { timeZone: 'America/New_York' });
 };
